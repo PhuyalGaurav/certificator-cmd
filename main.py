337e85e36bc38caeb8b49d569e6fde9988db04e3
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2024-02-03 19:59:08 +0545

    complete project, Add example files

--- main.py
+++ main.py
@@ -2,22 +2,42 @@ import csv
 import cv2
 import shutil
 import sys
+import argparse
 import os
 import zipfile
 from PIL import Image, ImageDraw, ImageFont
+argparse = argparse.ArgumentParser(description="Generate certificates from a template and a list of names")
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python main.py <template> <names> <font_size>")
+    try:
+        argparse.add_argument('--font', type=str, help='path for custom font')
+        argparse.add_argument('--size', type=int, help='font size')
+        argparse.add_argument('image', type=str, help='image file path')
+        argparse.add_argument('csv', type=str, help='csv file path')  
+        args = argparse.parse_args()
+        coordinates = get_click_coords(args.image)
+        if args.font:
+            font_file = args.font
+        else:    
+            font_file = "arial.ttf"
+        if args.size:
+            font_size = args.size
+        else:
+            font_size = 24
+        font = ImageFont.truetype(font_file, font_size)
+        generate_certificate(args.image, args.csv, coordinates, font)
+    except Exception as e:
+        print(e)
+        if str(e) == "cannot open resource":
+            print("File not found. Please check the file path and try again.")
+            sys.exit(1)
+        elif str(e) == "unknown file format":
+            print("Invalid file format. Please check the file and try again.")
+            sys.exit(1)
+        
+        print("Usage: python main.py <template> <names.csv> --font <font_path> --size <font_size>")
+        print("Example: python main.py template.png names.csv --font arial.ttf --size 24")
         sys.exit(1)
-
-    template_path = sys.argv[1]
-    names_path = sys.argv[2]
-    coordinates = get_click_coords(template_path)
-    font_size = int(sys.argv[3])
-
-    generate_certificate(template_path, names_path, coordinates, font_size)
-
 
 # Opens a window to click on the image and get the coordinates
 def get_click_coords(image_path):
@@ -49,9 +69,9 @@ def get_click_coords(image_path):
 
 
 # Generates the certificates and saves them in a zip file
-def generate_certificate(template_path, names_path, coordinates, font_size):
+def generate_certificate(template_path, names_path, coordinates, font):
 
-    font = ImageFont.truetype("arial.ttf", font_size)
+    
 
     path = "certificates"
     os.makedirs(path, exist_ok=True)
